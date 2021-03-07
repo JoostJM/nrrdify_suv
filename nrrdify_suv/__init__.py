7072a6d3cc16359362b20be27aebc16bf0dcf4ab
Joost van Griethuysen <joostjm@gmail.com>
2021-03-07 17:33:19 +0100

    ENH: Add Philips private tag conversion
    Philips defines tags (7053,1000), SUV conversion factor and (7053,1009), CNT conversion factor, to aid in conversion to SUV values from DICOM stored CNTS or BQML. Add code to use this private tag is it is defined and use default path otherwise. This also removes the requirement for Patient Weight, Series Time adn Radiopharmaceutical Information Sequence, as these are already incorporated into the SUV conversion factor, though the latter is still checked for to determine if the conversion should be attempted.

--- nrrdify_suv/__init__.py
+++ nrrdify_suv/__init__.py
@@ -13,6 +13,13 @@ import nrrdify
 from nrrdify import commandline
 import SimpleITK as sitk
 
+import pydicom
+
+pydicom.datadict.add_private_dict_entries('Philips PET Private Group', {  # {tag: (VR, VM, description) ...}
+   0x70531000: ('DS', '1', 'SUV Scale Factor'), 
+   0x70531009: ('DS', '1', 'Activity Concentration Scale Factor')
+})
+
 
 logger = logging.getLogger('nrrdify.suv')
 
@@ -45,48 +52,68 @@ def post_processing(im, slices):
       return im
 
   if not ('ATTN' in sanitychecks['CorrectedImage'] and ('DECAY' in sanitychecks['CorrectedImage'] or 'DECY' in sanitychecks['CorrectedImage']) and
-          (sanitychecks['DecayCorrection'] == 'START' or sanitychecks['DecayCorrection'] == 'ADMIN') and sanitychecks['Units'] == 'BQML'):
+          (sanitychecks['DecayCorrection'] == 'START' or sanitychecks['DecayCorrection'] == 'ADMIN')):
     logger.warning('Sanity checks failed for volume (patient %s, studydate %s series %d. %s), skipping...',
                    patient_name, study_date, series_number, series_description)
+    return im
 
-  required_tags = {}
-  required_base_keys = ('SeriesTime', 'PatientWeight')
-  required_seq_keys = ('RadionuclideHalfLife', 'RadionuclideTotalDose', 'RadiopharmaceuticalStartTime')
-
-  for k in required_base_keys:
-    required_tags[k] = getattr(slices[0], k, None)
-    if required_tags[k] is None:
-      logger.warning('Missing required tag "%s" in volume (patient %s, studydate %s series %d. %s), skipping...',
-                     k, patient_name, study_date, series_number, series_description)
+  if 0x70531000 in slices[0]:
+    if sanitychecks['Units'] == 'BQML':
+      SUV_conversion_factor = slices[0][0x70531000].value
+      CNT_conversion_factor = slices[0][0x70531009].value
+      SUV_constant = SUV_conversion_factor / CNT_conversion_factor
+      logger.info('Patient %s, studydate %s series %d. %s: Applying SUV conversion (SUV conversion constant %g, SUV conversion factor %g, parsed from (7053, 1000) / CNT conversion factor %g, parsed from (7053, 1009)',
+                   patient_name, study_date, series_number, series_description, SUV_constant, SUV_conversion_factor, CNT_conversion_factor)
+    elif sanitychecks['Units'] == 'CNTS':
+      SUV_constant = slices[0][0x70531000].value
+      logger.info('Patient %s, studydate %s series %d. %s: Applying SUV conversion (SUV conversion constant %g, parsed from (7053, 1000)',
+                   patient_name, study_date, series_number, series_description, SUV_constant)
+    else:
+      logger.warning('Expecting unit to be BQML or CNTS, skipping...')
       return im
-  for k in required_seq_keys:
-    required_tags[k] = getattr(radionuclide_sq[0], k, None)
-    if required_tags[k] is None:
-      logger.warning('Missing required tag "%s" in volume (patient %s, studydate %s series %d. %s), skipping...',
-                     k, patient_name, study_date, series_number, series_description)
+   
+  else:
+    if sanitychecks['Units'] != 'BQML':
+      logger.warning('Expecting unit to be BQML, skipping...')
       return im
+    required_tags = {}
+    required_base_keys = ('SeriesTime', 'PatientWeight')
+    required_seq_keys = ('RadionuclideHalfLife', 'RadionuclideTotalDose', 'RadiopharmaceuticalStartTime')
 
-  # Force cast to float
-  injected_dose = float(required_tags['RadionuclideTotalDose'])
-  bodyweight = float(required_tags['PatientWeight'])
-  half_life = float(required_tags['RadionuclideHalfLife'])
+    for k in required_base_keys:
+      required_tags[k] = getattr(slices[0], k, None)
+      if required_tags[k] is None:
+        logger.warning('Missing required tag "%s" in volume (patient %s, studydate %s series %d. %s), skipping...',
+                       k, patient_name, study_date, series_number, series_description)
+        return im
+    for k in required_seq_keys:
+      required_tags[k] = getattr(radionuclide_sq[0], k, None)
+      if required_tags[k] is None:
+        logger.warning('Missing required tag "%s" in volume (patient %s, studydate %s series %d. %s), skipping...',
+                       k, patient_name, study_date, series_number, series_description)
+        return im
 
-  if sanitychecks['DecayCorrection'] == 'START':  # images are decay-corrected to image acquisition time (additional correction for interval administration-acquisition is needed)
-    # Convert times to datetime and compute difference
-    series_time = datetime.datetime.strptime(required_tags['SeriesTime'], '%H%M%S')
-    admin_time = datetime.datetime.strptime(required_tags['RadiopharmaceuticalStartTime'], '%H%M%S')
-    decay_time = (series_time - admin_time).total_seconds()
+    # Force cast to float
+    injected_dose = float(required_tags['RadionuclideTotalDose'])
+    bodyweight = float(required_tags['PatientWeight'])
+    half_life = float(required_tags['RadionuclideHalfLife'])
 
-    # Compute total dose at acquisition start time
-    decayed_dose = injected_dose * (2 ** (-decay_time / half_life))
-  else:  # images are decay-corrected to administration time (so no additional correction needed)
-    decayed_dose = injected_dose
+    if sanitychecks['DecayCorrection'] == 'START':  # images are decay-corrected to image acquisition time (additional correction for interval administration-acquisition is needed)
+      # Convert times to datetime and compute difference
+      series_time = datetime.datetime.strptime(required_tags['SeriesTime'], '%H%M%S')
+      admin_time = datetime.datetime.strptime(required_tags['RadiopharmaceuticalStartTime'], '%H%M%S')
+      decay_time = (series_time - admin_time).total_seconds()
 
-  # Compute the SUV conversion factor
-  SUV_constant = bodyweight * 1000 / decayed_dose
+      # Compute total dose at acquisition start time
+      decayed_dose = injected_dose * (2 ** (-decay_time / half_life))
+    else:  # images are decay-corrected to administration time (so no additional correction needed)
+      decayed_dose = injected_dose
 
-  logger.info('Patient %s, studydate %s series %d. %s: Applying SUV conversion (SUV conversion constant %g, injected dose (at acquisition start time) %g, body weight %g)',
-              patient_name, study_date, series_number, series_description, SUV_constant, decayed_dose, bodyweight)
+    # Compute the SUV conversion factor
+    SUV_constant = bodyweight * 1000 / decayed_dose
+
+    logger.info('Patient %s, studydate %s series %d. %s: Applying SUV conversion (SUV conversion constant %g, injected dose (at acquisition start time) %g, body weight %g)',
+                patient_name, study_date, series_number, series_description, SUV_constant, decayed_dose, bodyweight)
 
   im = sitk.Cast(im, sitk.sitkFloat32)
   im *= SUV_constant
